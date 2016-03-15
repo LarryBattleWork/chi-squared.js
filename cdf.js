30b72273fd786c415686a424bbdc859818e826e8
Larry Battle <lbattle@spireon.com>
2016-03-15 14:43:43 -0500

    conformed to airbnb es6 eslint standard

--- cdf.js
+++ cdf.js
@@ -1,63 +1,57 @@
-var LogGamma = require('gamma').log
+const logGamma = require('gamma').log;
 
 // The following code liberated from
 // http://www.math.ucla.edu/~tom/distributions/chisq.html
 
-function Gcf(X,A) {        // Good for X>A+1
-  with (Math) {
-    var A0=0;
-    var B0=1;
-    var A1=1;
-    var B1=X;
-    var AOLD=0;
-    var N=0;
-    while (abs((A1-AOLD)/A1)>.00001) {
-      AOLD=A1;
-      N=N+1;
-      A0=A1+(N-A)*A0;
-      B0=B1+(N-A)*B0;
-      A1=X*A0+N*A1;
-      B1=X*B0+N*B1;
-      A0=A0/B1;
-      B0=B0/B1;
-      A1=A1/B1;
-      B1=1;
-    }
-    var Prob=exp(A*log(X)-X-LogGamma(A))*A1;
+function gcf(X, A) { // Good for X>A+1
+  let A0 = 0;
+  let B0 = 1;
+  let A1 = 1;
+  let B1 = X;
+  let AOLD = 0;
+  let N = 0;
+  while (Math.abs((A1 - AOLD) / A1) > 0.00001) {
+    AOLD = A1;
+    N = N + 1;
+    A0 = A1 + (N - A) * A0;
+    B0 = B1 + (N - A) * B0;
+    A1 = X * A0 + N * A1;
+    B1 = X * B0 + N * B1;
+    A0 = A0 / B1;
+    B0 = B0 / B1;
+    A1 = A1 / B1;
+    B1 = 1;
   }
-  return 1-Prob
+  const p = Math.exp(A * Math.log(X) - X - logGamma(A)) * A1;
+  return 1 - p;
 }
 
-function Gser(X,A) {        // Good for X<A+1.
-    with (Math) {
-    var T9=1/A;
-    var G=T9;
-    var I=1;
-    while (T9>G*.00001) {
-      T9=T9*X/(A+I);
-      G=G+T9;
-      I=I+1;
-    }
-    G=G*exp(A*log(X)-X-LogGamma(A));
-    }
-    return G
+function gser(X, A) { // Good for X<A+1.
+  let T9 = 1 / A;
+  let G = T9;
+  let I = 1;
+  while (T9 > G * 0.00001) {
+    T9 = T9 * X / (A + I);
+    G = G + T9;
+    I = I + 1;
+  }
+  G = G * Math.exp(A * Math.log(X) - X - logGamma(A));
+  return G;
 }
 
-function Gammacdf(x,a) {
-  var GI;
-  if (x<=0) {
-    GI=0
-  } else if (x<a+1) {
-    GI=Gser(x,a)
-  } else {
-    GI=Gcf(x,a)
+function gammacdf(x, a) {
+  if (x <= 0) {
+    return 0;
   }
-  return GI
+  if (x < a + 1) {
+    return gser(x, a);
+  }
+  return gcf(x, a);
 }
 
-module.exports = function (Z, DF) {
-  if (DF<=0) {
-    throw new Error("Degrees of freedom must be positive")
+module.exports = function cdf(Z, DF) {
+  if (DF <= 0) {
+    throw new Error('Degrees of freedom must be positive');
   }
-  return Gammacdf(Z/2,DF/2)
-}
+  return gammacdf(Z / 2, DF / 2);
+};
--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-var gamma = require('gamma');
+const gamma = require('gamma');
 
-exports.pdf = function (x, k_) {
-    if (x < 0) return 0;
-    var k = k_ / 2;
-    return 1 / (Math.pow(2, k) * gamma(k))
-        * Math.pow(x, k - 1)
-        * Math.exp(-x / 2)
-    ;
+exports.pdf = function pdf(x, k_) {
+  if (x < 0) return 0;
+  const k = k_ / 2;
+  const a = 1 / (Math.pow(2, k) * gamma(k));
+  const b = Math.pow(x, k - 1) * Math.exp(-x / 2);
+  return a * b;
 };
 
-exports.cdf = require('./cdf')
+exports.cdf = require('./cdf');
