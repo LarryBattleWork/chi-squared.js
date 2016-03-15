c06e655259b64f5f94815d40e410277c42da3837
Larry Battle <lbattle@spireon.com>
2016-03-15 18:09:47 -0500

    Added comments

--- cdf.js
+++ cdf.js
@@ -1,58 +1,60 @@
-"use strict";
+'use strict';
 const logGamma = require('gamma').log;
 
 // The following code liberated from
 // http://www.math.ucla.edu/~tom/distributions/chisq.html
 
-function gcf(X, A) { // Good for X>A+1
+// Good for X>A+1
+function gcf(X, A) {
   let A0 = 0;
   let B0 = 1;
   let A1 = 1;
   let B1 = X;
   let AOLD = 0;
   let N = 0;
-  while (Math.abs((A1 - AOLD) / A1) > 0.00001) {
+  const maxError = 0.00001;
+  while (maxError < Math.abs((A1 - AOLD) / A1)) {
+    N += 1;
     AOLD = A1;
-    N = N + 1;
     A0 = A1 + (N - A) * A0;
     B0 = B1 + (N - A) * B0;
     A1 = X * A0 + N * A1;
     B1 = X * B0 + N * B1;
-    A0 = A0 / B1;
-    B0 = B0 / B1;
-    A1 = A1 / B1;
+    A0 /= B1;
+    B0 /= B1;
+    A1 /= B1;
     B1 = 1;
   }
   const p = Math.exp(A * Math.log(X) - X - logGamma(A)) * A1;
   return 1 - p;
 }
-
-function gser(X, A) { // Good for X<A+1.
+// Good for X<A+1.
+function gser(X, A) {
   let T9 = 1 / A;
   let G = T9;
   let I = 1;
   while (T9 > G * 0.00001) {
-    T9 = T9 * X / (A + I);
-    G = G + T9;
-    I = I + 1;
+    T9 *= X / (A + I);
+    G += T9;
+    I += 1;
   }
-  G = G * Math.exp(A * Math.log(X) - X - logGamma(A));
-  return G;
+  const p = G * Math.exp(A * Math.log(X) - X - logGamma(A));
+  return p;
 }
 
-function gammacdf(x, a) {
-  if (x <= 0) {
-    return 0;
-  }
-  if (x < a + 1) {
-    return gser(x, a);
-  }
-  return gcf(x, a);
-}
-
+// Cumulative Distribution Function
 module.exports = function cdf(Z, DF) {
   if (DF <= 0) {
     throw new Error('Degrees of freedom must be positive');
   }
-  return gammacdf(Z / 2, DF / 2);
+  if (Z <= 0) {
+    return 0;
+  }
+  const x = Z / 2;
+  const a = DF / 2;
+  // aka z < df + 2
+  if (x < a + 1) {
+    return gser(x, a);
+  }
+  return gcf(x, a);
 };
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-"use strict";
 const gamma = require('gamma');
 
-exports.pdf = function pdf(x, k_) {
+// Probability Density Function
+exports.pdf = function pdf(x, df) {
   if (x < 0) return 0;
-  const k = k_ / 2;
-  const a = 1 / (Math.pow(2, k) * gamma(k));
-  const b = Math.pow(x, k - 1) * Math.exp(-x / 2);
-  return a * b;
+  if (df <= 0) {
+    throw new Error('Degrees of freedom must be positive');
+  }
+  const k = df / 2;
+  const a = Math.pow(x, k - 1) * Math.exp(-x / 2);
+  const b = Math.pow(2, k) * gamma(k);
+  return a / b;
 };
 
 exports.cdf = require('./cdf');
